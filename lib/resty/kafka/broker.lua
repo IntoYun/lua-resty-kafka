d6945da02cc9f9d859d5dfcae5ae7140b9a86daf
zhongjin <zhongjin@molmc.com>
2018-02-02 16:35:08 +0800

    add debug info

--- lib/resty/kafka/broker.lua
+++ lib/resty/kafka/broker.lua
@@ -20,12 +20,18 @@ local mt = { __index = _M }
 
 
 local function _send_receive(sock, payload)
+    ngx.log(ngx.DEBUG, "==> _send_receive()")
+    logger:log(ngx.DEBUG, "==> _send_receive()")
     local bytes, err = sock:send(payload)
+    ngx.log(ngx.DEBUG, "==> bytes: ", bytes, ", err: ", err)
+    logger:log(ngx.DEBUG, "==> bytes: ", bytes, ", err: ", err)
     if not bytes then
         return nil, err, true
     end
 
     local data, err = sock:receive(4)
+    ngx.log(ngx.DEBUG, "==> sock:receive(4) err: ", err)
+    logger:log(ngx.DEBUG, "==> sock:receive(4) data: ", data, ", err: ", err)
     if not data then
         if err == "timeout" then
             sock:close()
@@ -35,8 +41,11 @@ local function _send_receive(sock, payload)
     end
 
     local len = to_int32(data)
+    logger:log(ngx.DEBUG, "==> to_int32(data) len: ", len)
 
     local data, err = sock:receive(len)
+    ngx.log(ngx.DEBUG, "==> sock:receive(len) err: ", err)
+    logger:log(ngx.DEBUG, "==> sock:receive(len) data: ", data, ", err: ", err)
     if not data then
         if err == "timeout" then
             sock:close()
@@ -46,14 +55,17 @@ local function _send_receive(sock, payload)
     end
 
     if data == "" then -- send authBytes has no response
+        logger:log(ngx.DEBUG, "==> _send_receive() return true as authBytes resp")
         return true
     else
+        logger:log(ngx.DEBUG, "==> _send_receive() return resp, nil and true")
         return response:new(data), nil, true
     end
 end
 
 
 function _M.new(self, host, port, socket_config)
+    logger:log(ngx.DEBUG, "==> +++++ new a broker as agent... +++++ ")
     return setmetatable({
         host = host,
         port = port,
@@ -63,6 +75,7 @@ end
 
 
 local function sasl_plain_handshake(sock)
+    logger:log(ngx.DEBUG, "==> kafka sals plain handshake")
     local id = 0    -- hard code correlation_id
     local client_id = "worker" .. pid()
     local req = request:new(request.SaslHandshakeRequest, id, client_id)
@@ -94,6 +107,7 @@ end
 
 
 local function sasl_plain_auth(sock, config)
+    logger:log(ngx.DEBUG, "==> kafka sals plain auth with config: ", config)
     local ok, err = sasl_plain_handshake(sock)
     if not ok then
         return nil, err
@@ -120,26 +134,38 @@ end
 
 
 function _M.send_receive(self, req)
+    ngx.log(ngx.DEBUG, "==> broker:send_receive()")
+    logger:log(ngx.DEBUG, "==> broker:send_receive()")
     local sock, err = tcp()
     if not sock then
+        ngx.log(ngx.DEBUG, "==> broker:send_receive() not sock")
         return nil, err, true
     end
+    logger:log(ngx.DEBUG, "==> broker:send_receive() new a tcpsocket")
     sock:settimeout(self.config.socket_timeout)
 
     local ok, err = sock:connect(self.host, self.port)
     if not ok then
+        ngx.log(ngx.DEBUG, "==> broker:send_receive() sock:connect ok: ", ok)
         return nil, err, true
     end
 
+    ngx.log(ngx.DEBUG, "==> sock:getreusedtimes() returns: ", sock:getreusedtimes())
+    logger:log(ngx.DEBUG, "==> sock:getreusedtimes() returns: ", sock:getreusedtimes())
     if sock:getreusedtimes() == 0 and self.config.sasl_enable then
         local ok, err = sasl_plain_auth(sock, self.config)
         if not ok then
             return nil, err
         end
+        logger:log(ngx.DEBUG, "==> kafka sals plain auth done!")
     end
 
+    ngx.log(ngx.DEBUG, "==> broker:send_receive() call _send_receive()")
+    logger:log(ngx.DEBUG, "==> broker:send_receive() call _send_receive()")
     local resp, err, retryable = _send_receive(sock, req:package())
+    ngx.log(ngx.DEBUG, "==> broker:send_receive() call _send_receive() err: ", err)
 
+    logger:log(ngx.DEBUG, "==> sock:setkeepalive")
     sock:setkeepalive(self.config.keepalive_timeout, self.config.keepalive_size)
 
     return resp, err, retryable
